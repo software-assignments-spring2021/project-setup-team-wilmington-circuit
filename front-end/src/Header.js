6541e4554a43dcc4ed69399647c93c84d92d2207
m-ggie <maggiepiercea@gmail.com>
2021-03-12 23:22:05 -0500

    added header and menu

---
+++ front-end/src/Header.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import { Button, Nav, Navbar, NavItem, NavDropdown, NavLink, Form, FormControl } from 'react-bootstrap';
+import logo from './NYU-Logo-1.png'; 
+import avatar from './profile-pic.png';
+import HeaderProfile from './HeaderProfile'
+import './main.css';
+
+export default function Header() {
+    return (
+       <div class="profile-nav">
+           <Navbar bg="light" expand="lg">
+            <Navbar.Brand href="#home"><img src={logo} alt="NYU Logo" height="50px"></img></Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+                
+                <Nav className="mr-auto">
+                    <Nav.Link href="#home">My Courses</Nav.Link>
+                    <Nav.Link href="#link">Grades</Nav.Link>
+                    <Nav.Link href="#link">Finances</Nav.Link>
+                    <Nav.Link href="#link">Personal Information</Nav.Link>
+                </Nav>
+                <Nav>
+                    <HeaderProfile></HeaderProfile>
+                </Nav>
+            </Navbar.Collapse>
+            </Navbar>
+       </div> 
+    )
+}
