3486e80c37f6e82066f0f593740eb099fa028897
ajk745 <ajk745@nyu.edu>
2021-03-13 12:12:01 -0500

    fix calendar components

--- front-end/src/ScheduleCalendar.js
+++ front-end/src/ScheduleCalendar.js
@@ -1,44 +1,65 @@
-import { Calendar, momentLocalizer } from 'react-schedule-calendar';
-import moment from 'moment';
+import FullCalendar from '@fullcalendar/react';
+import dayGridPlugin from '@fullcalendar/daygrid';
+import timeGridPlugin from '@fullcalendar/timegrid';
+import listGridPlugin from '@fullcalendar/list';
+import bootstrapPlugin from '@fullcalendar/bootstrap';
+import 'bootstrap/dist/css/bootstrap.css';
+//import '@fortawesome/fontawesome-free/css/all.css'; // needs additional webpack config!
+
 import { getClassInfo } from './testData'
 import 'react-big-calendar/lib/css/react-big-calendar.css';
+import { useEffect, useState } from 'react';
 
-const localizer = momentLocalizer(moment);
+
  
 const ScheduleCalendar = props => {
-    const eventsList = [];
-    props.classIDs.forEach(id =>{
-        getClassInfo(id).then(classEvent => {
-            classEvent.meetings.forEach(meeting => {
-                
-                let currentMeeting = new Date(meeting.beginDate.split(' ').join('T'))
-                const end = new Date(meeting.endDate.split(' ').join('T'))
-                console.log(currentMeeting, end)
-                const duration = meeting.minutesDuration;
-
-                while(currentMeeting < end){
-                    const end = new Date(currentMeeting);
-                    end.setMinutes(end.getMinutes() + duration);
-                    eventsList.push({
-                        start: new Date(currentMeeting),
-                        end: end,
-                        title: classEvent.name
-                    })
-                    currentMeeting.setDate(currentMeeting.getDate() + 7);
-                    console.log(currentMeeting)
-                }
+    const [events, setEvents] = useState([])
+    useEffect(() =>{
+        props.classIDs.forEach(id =>{
+            getClassInfo(id).then(classEvent => {
+                classEvent.meetings.forEach(meeting => {
+                    
+                    let currentMeeting = new Date(meeting.beginDate.split(' ').join('T'))
+                    const end = new Date(meeting.endDate.split(' ').join('T'))
+                    const duration = meeting.minutesDuration;
+    
+                    while(currentMeeting < end){
+                        const end = new Date(currentMeeting);
+                        end.setMinutes(end.getMinutes() + duration);
+                        setEvents(prev => [...prev, {
+                            start: new Date(currentMeeting),
+                            end: end,
+                            title: classEvent.name
+                        }])
+                        currentMeeting.setDate(currentMeeting.getDate() + 7);
+                    }
+                })
             })
         })
-    })
-    console.log(eventsList)
+        console.log(events)
+    }, [])
+
     return (
         <div>
-          <Calendar
-            localizer={localizer}
-            events={eventsList}
-            startAccessor="start"
-            endAccessor="end"
-          />
+          <FullCalendar
+            plugins={[ dayGridPlugin, timeGridPlugin, listGridPlugin, bootstrapPlugin ]}
+            themeSystem='bootstrap'
+            buttonIcons={{
+                close: 'fa-times',
+                prev: 'fa-chevron-left',
+                next: 'fa-chevron-right',
+                prevYear: 'fa-angle-double-left',
+                nextYear: 'fa-angle-double-right'
+              }}
+            nowIndicator = {true}
+            headerToolbar={{
+              left: 'prev,next today',
+              center: 'title',
+              right: 'dayGridMonth,timeGridWeek,listWeek'
+            }}
+            events={events}
+            initialView="timeGrid"
+        />
         </div>
       )
 }
