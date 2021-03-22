bfb91a0154c5f8e3a853386bd568132cd1ee7324
Alex <ah4597@nyu.edu>
2021-03-22 16:45:17 -0400

    Add mock data for grades/personal

--- front-end/src/components/Grades.js
+++ front-end/src/components/Grades.js
@@ -1,136 +1,83 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import '../main.css';
 import { Button, ButtonToolbar, Dropdown, DropdownButton, ButtonGroup } from 'react-bootstrap';
 
-const sampleGrades = {
-    OverallGPA: '3.000',
-    MajorGPA: '3.500',
-    Terms: {
-        'Summer 2021': [
-            {
-                'name': 'Agile',
-                'id': 'CSCIUA 480',
-                'credits': '4',
-                'instructor': 'Amos Bloomberg',
-                'midterm-grade': 'A',
-                'final-grade': 'A'
-            },
-            {
-                'name': 'Agile',
-                'id': 'CSCIUA 480',
-                'credits': '4',
-                'instructor': 'Amos Bloomberg',
-                'midterm-grade': 'A',
-                'final-grade': 'A'
-            }
-        ],
-        'Spring 2021': [
-            {
-                'name': 'Agile',
-                'id': 'CSCIUA 480',
-                'credits': '4',
-                'instructor': 'Amos Bloomberg',
-                'midterm-grade': 'A',
-                'final-grade': 'A'
-            },
-            {
-                'name': 'Agile',
-                'id': 'CSCIUA 480',
-                'credits': '4',
-                'instructor': 'Amos Bloomberg',
-                'midterm-grade': 'A',
-                'final-grade': 'A'
-            },
-            {
-                'name': 'Agile',
-                'id': 'CSCIUA 480',
-                'credits': '4',
-                'instructor': 'Amos Bloomberg',
-                'midterm-grade': 'A',
-                'final-grade': 'A'
-            },
-            {
-                'name': 'Agile',
-                'id': 'CSCIUA 480',
-                'credits': '4',
-                'instructor': 'Amos Bloomberg',
-                'midterm-grade': 'A',
-                'final-grade': 'A'
-            }
-        ],
-        'Fall 2020': [
-            {
-                'name': 'Agile',
-                'id': 'CSCIUA 480',
-                'credits': '4',
-                'instructor': 'Amos Bloomberg',
-                'midterm-grade': 'A',
-                'final-grade': 'A'
-            },
-            {
-                'name': 'Agile',
-                'id': 'CSCIUA 480',
-                'credits': '4',
-                'instructor': 'Amos Bloomberg',
-                'midterm-grade': 'A',
-                'final-grade': 'A'
-            },
-            {
-                'name': 'Agile',
-                'id': 'CSCIUA 480',
-                'credits': '4',
-                'instructor': 'Amos Bloomberg',
-                'midterm-grade': 'A',
-                'final-grade': 'A'
-            }
-        ]
+let term = 0;
+export default class GradeInfo extends React.Component{
+
+    state = {
+        loading: true,
+        grades: null
     }
+
+    async componentDidMount() {
+        const url = 'https://my.api.mockaroo.com/grades_mock_data.json?key=202d5e00'
+        const response = await fetch(url);
+        const data = await response.json();
+        console.log(data);
+        this.setState({grades: data, loading: false})
+    }
+    
+    render() {
+        const setTerm = (newTerm) => {
+            if(newTerm == 'Spring 2021'){
+                term = 0;
+            } else if (newTerm == 'Fall 2020'){
+                term = 1;
+            } else  {
+                term = 2;
+            }
+            console.log(term);
+        }
+        return (
+            <div>
+                {this.state.loading || !this.state.grades ? (
+                    <div>loading...</div>
+                ) : (
+                <div>     
+                    <div class="row">
+                        <div className="topbar-container">
+                            <p>
+                                <b>Overall GPA:</b> {this.state.grades.overall_gpa}<br></br>
+                                Major GPA: {this.state.grades.major_gpa}
+                            </p>
+                            <ButtonToolbar className="justify-content-between">
+                                <ButtonGroup>
+                                <DropdownButton title='School'></DropdownButton>
+                                <DropdownButton id="dropdown-basic-button" title={this.state.grades.terms[0].term[term].time} onSelect={(term, e) => {
+                                    setTerm(term);
+                                    this.forceUpdate();
+                                }}>
+                                {this.state.grades.terms[0].term.map((term) =>{
+                                        return <Dropdown.Item eventKey={term.time}>{term.time}</Dropdown.Item>
+                                })}
+                                </DropdownButton>
+                                </ButtonGroup>
+                                <ButtonGroup>
+                                    <Button>Tran. PDF   </Button>
+                                </ButtonGroup>
+                            </ButtonToolbar>
+                            <p>
+                                {console.log(term)}
+                                Term GPA: {this.state.grades.terms[0].term[term].gpa} - Credits: {this.state.grades.terms[0].term[term].classes.reduce((total, course) =>{
+                                    return total += parseInt(course['credits']);
+                                }, 0)}<br></br>
+                                Junior - Computer Science
+                            </p>
+                            {this.state.grades.terms[0].term[term].classes.map(course => {
+                                return <div class="classElement card mx-auto border-light mb-3 mt-3" style={{ width: '95%', border_radius: 0}}>
+                                    {course['name']} ({course['id']}) - {course['credits']} Credits<br></br>
+                                    {course['instructor']}<br></br>
+                                    Midterm Grade: {course['midterm_grade']}<br></br>
+                                    <b>Final Grade: {course['final_grade']}</b>
+                                </div>
+                            })}
+                            
+                        </div>
+                    </div>
+                </div>)}
+            </div>
+        )
+    };
 }
 
-const GradeInfo = (props) => {
-    const [term, setTerm] = React.useState('Spring 2021');
-    return (
-        <div>
-            <div class="row">
-                <div className="topbar-container">
-                    <p>
-                        <b>Overall GPA:</b> {sampleGrades.OverallGPA}<br></br>
-                        Major GPA: {sampleGrades.MajorGPA}
-                    </p>
-                    <ButtonToolbar className="justify-content-between">
-                        <ButtonGroup>
-                        <DropdownButton title='School'></DropdownButton>
-                        <DropdownButton id="dropdown-basic-button" title={term} onSelect={(term, e) => {
-                        setTerm(term);
-                        }}>
-                        <Dropdown.Item eventKey="Fall 2020">Fall 2020</Dropdown.Item>
-                        <Dropdown.Item eventKey="Spring 2021">Spring 2021</Dropdown.Item>
-                        <Dropdown.Item eventKey="Summer 2021">Summer 2021</Dropdown.Item>
-                        </DropdownButton>
-                        </ButtonGroup>
-                        <ButtonGroup>
-                            <Button>Tran. PDF   </Button>
-                        </ButtonGroup>
-                    </ButtonToolbar>
-                    <p>
-                        Term GPA: 4.000 - Credits: {sampleGrades.Terms[term].reduce((total, course) =>{
-                            return total += parseInt(course['credits']);
-                        }, 0)}<br></br>
-                        Junior - Computer Science
-                    </p>
-                    {sampleGrades.Terms[term].map(course => {
-                        return <div class="classElement card mx-auto border-light mb-3 mt-3" style={{ width: '95%', border_radius: 0}}>
-                            {course['name']} ({course['id']}) - {course['credits']} Credits<br></br>
-                            {course['instructor']}<br></br>
-                            Midterm Grade: {course['midterm-grade']}<br></br>
-                            <b>Final Grade: {course['final-grade']}</b>
-                        </div>
-                    })}
-                    
-                </div>
-             </div>
-        </div>
-    )
-};
-
-export default GradeInfo;
--- front-end/src/components/Personal.js
+++ front-end/src/components/Personal.js
@@ -2,159 +2,168 @@ import React from 'react';
 import '../main.css';
 import avatar from '../images/profile-pic.png';
 
-const PersonalInfo = (props) => {
-    //im assuming props will be where user information is obtained? so like props.userID, props.addresses etc.?
-    // console.log(props);
-    return (
-        <div>
-            <div class="row">
-                <div class="col-lg-2"></div>
 
-                {/* Profile Pic */}
-                <div class="col-lg-2">
-                    <img align='left' src={avatar} alt="NYU Logo" height="200px"></img>
-                </div>
+export default class PersonalInfo extends React.Component{
 
-                {/* Basic Personal Info*/}
-                <div class="col-lg-2">
-                    <p align='left' style={{'font-size':'30px'}}>
-                        John Smith<br></br>
-                        He/Him<br></br>
-                        NID: N123456789 <br></br>
-                        DOB: 01/01/1991
-                    </p>
-                </div>
+    state = {
+        loading: true,
+        person: null
+    }
 
-                {/* Link to edit screen*/}
-                <div class='col-lg-4'>
-                    <a href='/personal#edit' style={{'float': 'right', 'font-size':'15px'}}><ins>Edit</ins></a>
-                </div>
-            </div> <br></br>
+    async componentDidMount() {
+        const url = 'https://my.api.mockaroo.com/mock_personal_data.json?key=202d5e00'
+        const response = await fetch(url);
+        const data = await response.json();
+        console.log(data);
+        this.setState({person: data, loading: false})
+    }
 
-            {/* Addresses header */}
-            <div class="row">
-                <div class="col-lg-2"></div>
-                <div class="col-lg-8">
-                    <h2 align='left' style={{'font-size':'25px'}}>
-                        Addresses
-                    </h2>
-                </div>
-            </div>
-
-            {/* Addresses row*/}
-            <div class="row">
-                <div class="col-lg-2"></div>
-                <div class="col-lg-4">
-                    <p align='left'>
-                        <b>Mailing</b><br></br>
-                        123 Jane St<br></br>
-                        New York, NY<br></br>
-                        10014 USA
-                    </p> 
-                </div>
-                <div class="col-lg-4">
-                    <p align='left'>
-                        <b>Permanent</b><br></br>
-                        111 North Piney Rd<br></br>
-                        Pierre, SD <br></br>
-                        57501 USA
-                    </p>
-                </div>
-            </div>
-
-            {/* Phone Numbers Header */}
-            <div class="row">
-                <div class="col-lg-2"></div>
-                <div class="col-lg-8">
-                    <h2 align='left' style={{'font-size':'25px'}}>
-                        Phone Numbers
-                    </h2>
-                </div>
-            </div>
+    render() {
+        return (
+            <div>
+                {this.state.loading || !this.state.person ? (
+                    <div>loading...</div>
+                ) : (
+                    <div>
+                        <div class="row">
+                            <div class="col-lg-2"></div>
             
-            {/* Phone Numbers row */}
-            <div class="row">
-                <div class="col-lg-2"></div>
-                <div class="col-lg-2">
-                    <p align='left'>
-                        <b>Mobile</b><br></br>
-                        (123) 456-7890<br></br>
-                    </p> 
+                            {/* Profile Pic */}
+                            <div class="col-lg-2">
+                                <img align='left' src={avatar} alt="NYU Logo" height="200px"></img>
+                            </div>
+            
+                            {/* Basic Personal Info*/}
+                            <div class="col-lg-2">
+                                <p align='left' style={{'font-size':'30px'}}>
+                                    {this.state.person.first_name} {this.state.person.last_name}<br></br>
+                                    He/Him<br></br>
+                                    NID: N{this.state.person.NID}<br></br>
+                                    DOB: {this.state.person.dob}
+                                </p>
+                            </div>
+            
+                            {/* Link to edit screen*/}
+                            <div class='col-lg-4'>
+                                <a href='/personal#edit' style={{'float': 'right', 'font-size':'15px'}}><ins>Edit</ins></a>
+                            </div>
+                        </div> <br></br>
+            
+                        {/* Addresses header */}
+                        <div class="row">
+                            <div class="col-lg-2"></div>
+                            <div class="col-lg-8">
+                                <h2 align='left' style={{'font-size':'25px'}}>
+                                    Addresses
+                                </h2>
+                            </div>
+                        </div>
+            
+                        {/* Addresses row*/}
+                        <div class="row">
+                            <div class="col-lg-2"></div>
+                            {this.state.person.addresses.map((address) =>{
+                                return (
+                                    <div class="col-lg-4">
+                                        <p align='left'>
+                                            <b>{address.type}</b><br></br>
+                                            {address.street_address}<br></br>
+                                            {address.city}, {address.state}<br></br>
+                                            {address.postal_code} {address.country}
+                                        </p>
+                                    </div>
+                                )
+                            })}
+                        </div>
+            
+                        {/* Phone Numbers Header */}
+                        <div class="row">
+                            <div class="col-lg-2"></div>
+                            <div class="col-lg-8">
+                                <h2 align='left' style={{'font-size':'25px'}}>
+                                    Phone Numbers
+                                </h2>
+                            </div>
+                        </div>
+                        
+                        {/* Phone Numbers row */}
+                        <div class="row">
+                            <div class="col-lg-2"></div>
+                            {this.state.person.phone_numbers.map((number) =>{
+                                return (
+                                    <div class="col-lg-2">
+                                        <p align='left'>
+                                            <b>{number.type}</b><br></br>
+                                            {number.number}<br></br>
+                                        </p>
+                                    </div>
+                                )
+                            })}
+                        </div>
+            
+                        {/* Email Addresses Header */}
+                        <div class="row">
+                            <div class="col-lg-2"></div>
+                            <div class="col-lg-8">
+                                <h2 align='left' style={{'font-size':'25px'}}>
+                                    Email Addresses
+                                </h2>
+                            </div>
+                        </div>
+            
+                        {/* Email Addresses row */}
+                        <div class="row">
+                            <div class="col-lg-2"></div>
+                            {this.state.person.emails.map((email) =>{
+                                return (
+                                    <div class="col-lg-2">
+                                        <p align='left'>
+                                            <b>{email.type}</b><br></br>
+                                            {email.email}<br></br>
+                                        </p>
+                                    </div>
+                                )
+                            })}
+                        </div>
+            
+                        {/* Emergency Contact Header */}
+                        <div class="row">
+                            <div class="col-lg-2"></div>
+                            <div class="col-lg-8">
+                                <h2 align='left' style={{'font-size':'25px'}}>
+                                    Emergency Contact
+                                </h2>
+                            </div>
+                        </div>
+            
+                        {/* Emergency Contact row */}
+                        <div class="row">
+                            <div class="col-lg-2"></div>
+                            {this.state.person.emergency_contacts.map((contact) =>{
+                                return (
+                                    <div class="col-lg-2">
+                                        <p align='left'>
+                                            <b>{contact.first_name} {contact.last_name} - {contact.relationship}</b><br></br>
+                                            {contact.phone_number}<br></br>
+                                        </p>
+                                    </div>
+                                )
+                            })}
+                        </div>
+            
+                        {/* Citizenship header + status */}
+                        <div class="row">
+                            <div class="col-lg-2"></div>
+                            <div class="col-lg-8">
+                                <h2 align='left' style={{'font-size':'25px'}}>
+                                    Citizenship
+                                </h2>
+                                <p align='left'>{this.state.person.citizenship_status}</p>
+                            </div>
+                        </div>
+                    </div>)}
                 </div>
-                <div class="col-lg-2">
-                    <p align='left'>
-                        <b>Local</b><br></br>
-                        (098) 765-4321<br></br>
-                    </p>
-                </div>
-                <div class="col-lg-2">
-                    <p align='left'>
-                        <b>NYU Emergency Alert</b><br></br>
-                        (456) 321-0789
-                    </p>
-                </div>
-            </div>
-
-            {/* Email Addresses Header */}
-            <div class="row">
-                <div class="col-lg-2"></div>
-                <div class="col-lg-8">
-                    <h2 align='left' style={{'font-size':'25px'}}>
-                        Email Addresses
-                    </h2>
-                </div>
-            </div>
-
-            {/* Email Addresses row */}
-            <div class="row">
-                <div class="col-lg-2"></div>
-                <div class="col-lg-2">
-                    <p align='left'>
-                        <b>Personal</b><br></br>
-                        XXX@XXX.XXX<br></br>
-                    </p> 
-                </div>
-                <div class="col-lg-2">
-                    <p align='left'>
-                        <b>NYU Net ID</b><br></br>
-                        XXX@XXX.XXX<br></br>
-                    </p>
-                </div>
-            </div>
-
-            {/* Emergency Contact Header */}
-            <div class="row">
-                <div class="col-lg-2"></div>
-                <div class="col-lg-8">
-                    <h2 align='left' style={{'font-size':'25px'}}>
-                        Emergency Contact
-                    </h2>
-                </div>
-            </div>
-
-            {/* Emergency Contact row */}
-            <div class="row">
-                <div class="col-lg-2"></div>
-                <div class="col-lg-2">
-                    <p align='left'>
-                        <b>James Smith - Father</b><br></br>
-                        (XXX) XXX-XXXX<br></br>
-                    </p> 
-                </div>
-            </div>
-
-            {/* Citizenship header + status */}
-            <div class="row">
-                <div class="col-lg-2"></div>
-                <div class="col-lg-8">
-                    <h2 align='left' style={{'font-size':'25px'}}>
-                        Citizenship
-                    </h2>
-                    <p align='left'>US Citizen</p>
-                </div>
-            </div>
-        </div>
-    )
-};
-
-export default PersonalInfo;
+            
+        )
+    }
+}
