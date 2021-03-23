39c8c57bb5bb6dca8bc8a8ad5d2986e702f757f1
m-ggie <maggiepiercea@gmail.com>
2021-03-23 17:45:33 -0400

    margins at top and fixed input background color

--- front-end/src/components/Login.js
+++ front-end/src/components/Login.js
@@ -3,31 +3,33 @@ import React from 'react';
 export default function Login() {
    
         return (
-            <form class='mx-auto' style={{ width: '50%'}}>
-                <h3>Log In to Your NYU Account</h3>
+            <div class="login-pg">
+                <form class='mx-auto' style={{ width: '50%'}}>
+                    <h3>Log In to Your NYU Account</h3>
 
-                <div className="form-group">
-                    <label>NetID</label>
-                    <input type="email" className="form-control"/>
-                </div>
-
-                <div className="form-group">
-                    <label>Password</label>
-                    <input type="password" className="form-control"/>
-                </div>
-
-                <div className="form-group">
-                    <div className="custom-control custom-checkbox">
-                        <input type="checkbox" className="custom-control-input" id="customCheck1" />
-                        <label className="custom-control-label" htmlFor="customCheck1">Remember me</label>
+                    <div className="form-group">
+                        <label>NetID</label>
+                        <input type="email" className="form-control"/>
                     </div>
-                </div>
 
-                <button type="submit" className="btn btn-primary btn-block">Log In</button>
-                <p className="forgot-password text-right">
-                    <a href="/">Forgot Password</a>
-                </p>
-                
-            </form>
+                    <div className="form-group">
+                        <label>Password</label>
+                        <input type="password" className="form-control"/>
+                    </div>
+
+                    <div className="form-group">
+                        <div className="custom-control custom-checkbox">
+                            <input type="checkbox" className="custom-control-input" id="customCheck1" />
+                            <label className="custom-control-label" htmlFor="customCheck1">Remember me</label>
+                        </div>
+                    </div>
+
+                    <button type="submit" className="btn btn-primary btn-block" href="/">Log In</button>
+                    <p className="forgot-password text-right">
+                        <a href="/">Forgot Password</a>
+                    </p>
+                    
+                </form>
+            </div>
         );
     }
