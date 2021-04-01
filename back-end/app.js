7cd41f93572ebbdc793ad177e549796fdd61ccf2
ajk745 <ajk745@nyu.edu>
2021-04-01 17:52:50 -0400

    Clear repo :(

--- back-end/app.js
+++
@@ -1,9 +0,0 @@
-// import and instantiate express
-const express = require("express") // CommonJS import style!
-const app = express() // instantiate an Express object
-
-app.get('/', (req, res)=>{
-    res.send('Test server')
-})
-
-module.exports = app
