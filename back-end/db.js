ef251fbb99d57522690eacff2fccf39bb8a2d1be
Santiago Darré <santiagodarre@disroot.org>
2021-04-12 15:00:59 -0400

    db setup

---
+++ back-end/db.js
@@ -0,0 +1,32 @@
+const mongoose = require("mongoose");
+
+const User = new mongoose.Schema({
+	"google_id": {type: String, required: true}, // 'Aa' from Google account obj
+	"name": {type: String},
+	"locations": [String], // stored as coords
+	"friends": [String], // stored as google_id
+	"groups": [String] // stored as group_id
+}, {collection: "User"});
+mongoose.model("User", User);
+
+const Group = new mongoose.Schema({
+	"group_id": {type: String, required: true},
+	"name": {type: String},
+	"locations": [String], // stored as coords
+	"members": [String] // stored as google_id
+}, {collection: "Group"});
+mongoose.model("Group", Group);
+
+const db = (process.env.NODE_ENV === "PRODUCTION") ?
+	`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.w3xow.mongodb.net/myFirstDatabase?retryWrites=true&w=majority` :
+	`mongodb://localhost/MeetMe`;
+
+const connect = () => {
+	mongoose.connect(db, {useNewUrlParser: true});
+};
+
+const disconnnect = () => {
+	mongoose.disconnect();
+};
+
+module.exports = {connect, disconnect};
