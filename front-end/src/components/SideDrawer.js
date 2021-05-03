3faec06525862d9d41b8336bef4c6e2b97b9f325
m-ggie <maggiepiercea@gmail.com>
2021-05-03 13:28:24 -0400

    added about me

--- front-end/src/components/SideDrawer.js
+++ front-end/src/components/SideDrawer.js
@@ -1,5 +1,5 @@
 import React, { useState, useRef, useEffect } from 'react'
-import { Button } from 'react-bootstrap'
+import { Button, Modal } from 'react-bootstrap'
 import './SideDrawer.css'
 import Profile from './Profile'
 import getMockData from '../testData';
@@ -30,6 +30,12 @@ const SideDrawer = function (props) {
     const [toggle, setToggle] = useState(false);
     const [toggle2, setToggle2] = useState(false);
     const [toggle3, setToggle3] = useState(false);
+
+    const [show, setShow] = useState(false);
+
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
+  
 
     const [friends, setFriend] = useState([]);
     const [groups, setGroup] = useState([]);
@@ -62,12 +68,17 @@ const SideDrawer = function (props) {
    
     return (
         <nav className={drawerClasses} ref={myRef} onClick={handleClickInside}>
+            
+            
             {props.user ?
             <ul>
+                
+            
+           
                 <li><Profile user={props.user} /></li>
 
                 <div>
-                    <li><a onClick={() => setToggle(!toggle)}>Friends +</a></li>
+                    <li><a onClick={() => setToggle(!toggle)}><h5>Friends +</h5></a></li>
                     {toggle && (
                         <div>
                             <ul>
@@ -92,7 +103,7 @@ const SideDrawer = function (props) {
                         setToggle2(!toggle2);
                         setToggle3(false);
                     }
-                    }>Groups +</a>{toggle2 && (<><Button className='edit' onClick={() => {
+                    }><h5>Groups +</h5></a>{toggle2 && (<><Button className='edit' onClick={() => {
                         setToggle3(!toggle3);
                         setToggle2(true);
                     }}>{toggle3 ? 'Done' : 'Edit'}</Button>
@@ -125,8 +136,34 @@ const SideDrawer = function (props) {
                             </ul>
                         </div>
                     )}
+                    <li onClick={handleShow}> <h5>About Meet Me</h5> </li>
+                {show && (
+                   <Modal show={show} onHide={handleClose} animation={false}>
+                   <Modal.Header closeButton>
+                     <Modal.Title>About Meet Me</Modal.Title>
+                   </Modal.Header>
+                   <Modal.Body><p>Meet Me was created by Alex Hammer, Ari Khaytser, Luis Cordova, Santiago Darré, 
+                       and Maggie Pierce for their Agile Development class. Meet Me is intended for people who want to find a meeting point for them
+                       and their friends or colleagues. </p>
+                    
+
+                       <p>To use Meet Me, enter in at least two starting locations, select any filter you want, and click search. Meet me will calculate 
+                       the center point of the entered locations and return results nearby. If you wish to save groups or commonly used locations, you can do so after
+                       creating an account. You don't need an account to access Meet Me's basic functionality.</p>
+                       
+                       <p>For more information, <a href="https://github.com/agile-dev-assignments/project-setup-team-wilmington-circuit" target="_blank">visit the project GitHub</a>.</p>
+                       </Modal.Body>
+                   <Modal.Footer>
+                     <Button variant="secondary" onClick={handleClose}>
+                       Close
+                     </Button>
+                   </Modal.Footer>
+                 </Modal>
+               
+                )}
                 </div>
             </ul>
+            
             :
             <div class="logged-out-text">
                 <h3>Log in to save locations, friends, and more!</h3>
